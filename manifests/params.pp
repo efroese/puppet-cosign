098b9c403004caebcd0a299aadcac6279b3590a6
Erik Froese <erik.froese@gmail.com>
2012-07-31 10:17:31 -0400

    Remove unused params.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,3 @@
 class cosign::params {
-
-    Class['Apache::Params'] -> Class['Cosign::Params']
-
-    $apache_modules_dir = $::operatingsystem ? { 
-        /RedHat|CentOS|Amazon/ => '/usr/lib64/httpd/modules',
-        /Debian|Ubuntu/        => '/usr/lib/apache2/modules',
-    }
-
-    $ssl_dir = $::operatingsystem ? { 
-        /RedHat|CentOS|Amazon/ => '/etc/httpd/ssl',
-        /Debian|Ubuntu/        => '/etc/apache2/ssl',
-    }
-
     $source = "/var/cache/cosign/source"
 }
